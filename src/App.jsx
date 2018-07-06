68ae032cee1b8b72a811445726aae8b93e191d55
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2018-07-07 01:49:12 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="row vertical-offset-100">
+        <div className="col-md-4 col-md-offset-4">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h3 className="panel-title">Find </h3>
+            </div>
+            <div className="panel-body">
+              <form accept-charset="UTF-8" role="form">
+                <div className="form-group">
+                  <input className="form-control" placeholder="Enter GitHub username" name="email" type="text" />
+                </div>
+                  <input className="btn btn-lg btn-success btn-block" type="submit" value="Login" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+class Form extends React.Component{
+  render(){
+    return(
+      <div className="col-md-4">
+        <form>
+          <input type="text" className="form-control" placeholder="Enter Github username" />
+        </form>
+      </div>
+    );
+  }
+} 
 
 export default App;
